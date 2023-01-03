d88701e2f9fe25a2f8207ae0a12b69e8f5086f65
romeo <rjameson1928@gmail.com>
2023-01-03 22:32:14 +0300

    lesson 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,33 @@
 import React from 'react';
 import './App.css';
+import {Todolist} from "./Todolist";
 
 function App() {
-    return (
-        <div className="App">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
-        </div>
-    );
+
+	const shapka = 'What to learn';
+	// const shapka2 = 'shapka2';
+	const shapka3 = 'What to learn222222';
+
+	const tasks1 = [
+		{id: 1, title: "HTML&CSS", isDone: true}, //0
+		{id: 2, title: "JS", isDone: true},       //1
+		{id: 3, title: "ReactJS", isDone: false}  //2
+	]
+	const tasks2 = [
+		{id: 1, title: "Hello world", isDone: true}, //0
+		{id: 2, title: "I am Happy", isDone: false}, //1
+		{id: 3, title: "Yo", isDone: false},         //2
+		{id: 4, title: "HYo", isDone: false}         //3
+	]
+
+
+	return (
+		<div className="App">
+			<Todolist shapka={shapka} tasks={tasks1}/> {/*shapka2 = {'shapka2'}*/}
+			<Todolist shapka={shapka3} tasks={tasks2}/>
+		</div>
+	);
 }
 
 export default App;
+
